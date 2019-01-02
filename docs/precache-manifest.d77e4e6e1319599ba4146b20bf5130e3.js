add79e2218a813a69da5b72a145e9f0d1277db5b
fhagno@yahoo.com <fhagno@yahoo.com>
2019-01-02 18:13:24 +0100

    Adding icons

---
+++ docs/precache-manifest.d77e4e6e1319599ba4146b20bf5130e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "cd9b8259cedb97e74287",
+    "url": "/robofriends/static/js/main.cd9b8259.chunk.js"
+  },
+  {
+    "revision": "c5ca306852932c39d1ed",
+    "url": "/robofriends/static/js/1.c5ca3068.chunk.js"
+  },
+  {
+    "revision": "cd9b8259cedb97e74287",
+    "url": "/robofriends/static/css/main.3c35cc5e.chunk.css"
+  },
+  {
+    "revision": "c5ca306852932c39d1ed",
+    "url": "/robofriends/static/css/1.db33156f.chunk.css"
+  },
+  {
+    "revision": "a4d5aca7c5b5651310879830de032c7c",
+    "url": "/robofriends/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -6,15 +6,15 @@
  * See https://goo.gl/nhQhGp
  *
  * The rest of the code is auto-generated. Please don't update this file
- * directly; instead, make changes to your Workbox docs configuration
- * and re-run your docs process.
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
  * See https://goo.gl/2aRDsh
  */
 
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2ecd89bc96e846db4b052690dfee38c1.js"
+  "/robofriends/precache-manifest.d77e4e6e1319599ba4146b20bf5130e3.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/robofriends/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
